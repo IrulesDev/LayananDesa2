03c6711b8ba5bd3d82dbaacbf8f94989800cd906
IrulesDev <ad4112ul@gmail.com>
2025-02-10 09:49:25 +0700

    tambah pages

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',function(){
+    return view('auth.login');
+});
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', function () {
+        return view('pages.dashboard');
+    });
+    Route::get('/', function () {
+        return view('pages.dashboard');
+    });
 });
