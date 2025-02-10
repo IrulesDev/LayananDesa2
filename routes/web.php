289aed89f24674d0b536aaecdfa417f3f6b9715f
IrulesDev <ad4112ul@gmail.com>
2025-02-10 09:59:40 +0700

    route/web

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',function(){
-    return view('auth.login');
-});
+// Route::get('/',function(){
+//     return view('auth.login');
+// });
 
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/', function () {
+    Route::get('/home', function () {
         return view('pages.dashboard');
     });
     Route::get('/', function () {
