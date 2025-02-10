de8139837eb98134339fdddb5305ab7b1406eb18
IrulesDev <ad4112ul@gmail.com>
2025-02-10 09:08:48 +0700

    memanipulasi login agar tidakbisa masuk ke dashboard

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PengajuanController;
+use App\Http\Controllers\LayananController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\KeluargaController;
+use App\Http\Controllers\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('login', [AuthController::class, 'login'])->name('api.login');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::resource('user', UserController::class);
+    Route::resource('pengajuan', PengajuanController::class);
+    Route::resource('layanan', LayananController::class);
+    Route::post('logout', [AuthController::class, 'logout'])->name('api.logout');
+});
