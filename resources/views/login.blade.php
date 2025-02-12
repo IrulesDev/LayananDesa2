1de2725d52752f5213307755331e4b61d60ac32b
IrulesDev <ad4112ul@gmail.com>
2025-02-12 09:35:11 +0700

    pembaruan password

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -41,45 +41,37 @@
                         </h4>
                         <p class="text-muted">Before you get started, you must login or register if you don't already
                             have an account.</p>
-                        <form method="POST"
-                            action="{{ route('login') }}"
-                            class="needs-validation"
-                            novalidate="">
+                        <form method="POST"action="{{ route('login') }}" class="needs-validation" novalidate="">
                             @csrf
+
                             <div class="form-group">
                                 <label for="email">Email</label>
-                                <input id="email"
-                                    type="email"
-                                    class="form-control"
-                                    @error('email') is-invalid @enderror
-                                    name="email"
-                                    tabindex="1"
-                                    required
-                                    autofocus>
-                                    @error('email')
+                                <input id="email" type="email" class="form-control" @error('email') is-invalid @enderror name="email"
+                                tabindex="1" required autofocus>
+                            @error('email')
                                 <div class="invalid-feedback">
                                     {{ $message }}
                                 </div>
-                                @enderror
+                            @enderror
                             </div>
 
                             <div class="form-group">
-                                <div class="d-block">
-                                    <label for="password"
-                                        class="control-label">Password</label>
+                                <label for="password" class="control-label">Password</label>
+                                <div class="input-group">
+                                    <input id="password" type="password" @error('password') is-invalid @enderror class="form-control" name="password" 
+                                    placeholder="Enter your password" tabindex="2" required>
+                                    <div class="input-grub-prepend">
+                                        <div class="input-grub-text">
+                                            <i class="fas fa-eye" id="togglePassword" style="cursor: pointer;">
+                                            </i>
+                                        </div>
+                                    </div>
                                 </div>
-                                <input id="password"
-                                    type="password"
-                                    @error('password') is-invalid @enderror
-                                    class="form-control"
-                                    name="password"
-                                    tabindex="2"
-                                    required>
                                     @error('password')
                                 <div class="invalid-feedback">
                                     {{ $message }}
                                 </div>
-                                @enderror   
+                            @enderror   
                             </div>
 
                             <div class="form-group">
